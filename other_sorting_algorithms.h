8646b661d156102c7ff1b351e1344c374aaa6665
Ivan <ivansandev82@gmail.com>
2020-06-18 17:52:28 +0200

    comments

--- other_sorting_algorithms.h
+++ other_sorting_algorithms.h
@@ -3,7 +3,6 @@ using namespace std;
 
 void swap(int *a, int *b)
 {
-   // Used for swapping elements in the heap (also called sifting the elements up/down)
    int temp;
    temp = *a;
    *a = *b;
@@ -12,8 +11,8 @@ void swap(int *a, int *b)
 
 int partition(int arr[], int low, int high)
 {
-   int pivot = arr[high]; // pivot
-   int i = (low - 1);     // Index of smaller element
+   int pivot = arr[high];
+   int i = (low - 1);
 
    for (int j = low; j <= high - 1; j++)
    {
@@ -49,8 +48,6 @@ void selectionSort(int array[], int size)
          if (array[i] > array[min_idx])
             min_idx = i;
       }
-
-      // put min at the correct position
       swap(&array[min_idx], &array[step]);
    }
 }
@@ -81,19 +78,16 @@ void merge(int arr[], int l, int m, int r)
    int n1 = m - l + 1;
    int n2 = r - m;
 
-   /* create temp arrays */
    int L[n1], R[n2];
 
-   /* Copy data to temp arrays L[] and R[] */
    for (i = 0; i < n1; i++)
       L[i] = arr[l + i];
    for (j = 0; j < n2; j++)
       R[j] = arr[m + 1 + j];
 
-   /* Merge the temp arrays back into arr[l..r]*/
-   i = 0; // Initial index of first subarray
-   j = 0; // Initial index of second subarray
-   k = l; // Initial index of merged subarray
+   i = 0;
+   j = 0;
+   k = l;
    while (i < n1 && j < n2)
    {
       if (L[i] <= R[j])
@@ -109,8 +103,6 @@ void merge(int arr[], int l, int m, int r)
       k++;
    }
 
-   /* Copy the remaining elements of L[], if there 
-       are any */
    while (i < n1)
    {
       arr[k] = L[i];
@@ -118,8 +110,6 @@ void merge(int arr[], int l, int m, int r)
       k++;
    }
 
-   /* Copy the remaining elements of R[], if there 
-       are any */
    while (j < n2)
    {
       arr[k] = R[j];
@@ -128,20 +118,13 @@ void merge(int arr[], int l, int m, int r)
    }
 }
 
-/* l is for left index and r is right index of the 
-   sub-array of arr to be sorted */
 void mergeSort(int arr[], int l, int r)
 {
    if (l < r)
    {
-      // Same as (l+r)/2, but avoids overflow for
-      // large l and h
       int m = l + (r - l) / 2;
-
-      // Sort first and second halves
       mergeSort(arr, l, m);
       mergeSort(arr, m + 1, r);
-
       merge(arr, l, m, r);
    }
 }
